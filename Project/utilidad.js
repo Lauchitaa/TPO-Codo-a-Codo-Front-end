a50d6dfeddbf9b095eb9b8bd011e5f5767e2434d
Lauchitaa <llloret.job@gmail.com>
2023-05-17 17:07:20 -0300

    Pagina con animaciones, faltan detalles y form

--- Project/utilidad.js
+++ Project/utilidad.js
@@ -1,6 +1,31 @@
+
 fetch('https://api.ejemplo.com/recurso', {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
   }
 })
+
+window.sr = ScrollReveal();
+  sr.reveal('.sr', {
+    duration: 2500,
+    origin:"top",
+    distance:'-200px',
+    easing: 'ease-in-out',
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.izq', {
+    duration: 3000,
+    origin:"left",
+    distance: '-300px',
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.der', {
+    duration: 3000,
+    origin:"right",
+    distance: '-300px'
+  });
+
+ 
